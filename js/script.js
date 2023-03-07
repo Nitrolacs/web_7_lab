0490e05a1209415f8b12f17edb200e76d838e4c5
Terentev Nikita <nitro-2003@mail.ru>
2023-03-07 12:31:19 +0700

    Added img animation

--- js/script.js
+++ js/script.js
@@ -21,9 +21,10 @@ document.links.history.ondblclick = function() {
 	console.log('Была два раза нажата кнопка перехода на историю электромобилей.');
 }
 
-document.links.tesla.onmouseover = function() {
+function tesla() {
 	console.log('Была наведена мышка на кнопку для перехода на сайт теслы.')
-}
+} 
+document.links.tesla.onmouseover = tesla;
 
 // Регистрация обработчиков
 let outLink = document.getElementById("out_link");
@@ -31,3 +32,21 @@ outLink.addEventListener("mousedown", function(e) {console.log(e);}, false)
 
 let outImg = document.getElementById("out_img");
 outImg.addEventListener("mouseout", function(e) {console.log(e);}, false)
+
+// Перелистывание картинки по нажатию
+let index = 0;
+let imgs = Array("img/nissan_leaf.jpg", "img/nissan_leaf_another.jpg", "img/nissan_leaf_snow.jpg");
+let nissanLeafImg = document.images["leaf"];
+
+function swapImages() {
+	if (index + 1 != imgs.length) {
+		index++;
+		nissanLeafImg.src=imgs[index];
+	}
+	else {
+		index = 0;
+		nissanLeafImg.src=imgs[index];
+	}
+}
+
+nissanLeafImg.addEventListener("click", swapImages, false)
